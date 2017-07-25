a79d1c2fc3431391cc5c2c8e0863563bb340018e
alihass3 <hasan.ali.mnpk202@gmail.com>
2017-07-25 17:20:51 -0400

    index view completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,29 +5,29 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @products = Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def new
-    @products = Product.new
+    @product = Product.new
   end
 
   def create
-    @products = Product.new
+    @product = Product.new
 
-    @products.name = params[:product][:name]
-    @products.description = params[:product][:description]
-    @products.price = params[:product][:price]
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price = params[:product][:price]
 
 
-    if @products.save
+    if @product.save
       # if the picture gets saved, generate a get request to "/pictures" (the index)
       redirect_to "/products" #would take it to show page of index
     else
       # otherwise render new.html.erb
       render :new
     end
-    
+
   end
 
 
